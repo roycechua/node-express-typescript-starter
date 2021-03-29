50f74d5ccafb87e3cd1d788f30c8f72d81c32e17
Royce Chua <royce236@gmail.com>
2021-03-29 22:18:47 +0800

    Initial commit

---
+++ index.ts
@@ -0,0 +1,24 @@
+import express, { Application, Request, Response } from "express";
+
+const app: Application = express();
+const port = 3000;
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.get(
+	"/",
+	async (req: Request, res: Response): Promise<Response> => {
+		return res.status(200).send({
+			message: "Hello World!",
+		});
+	}
+);
+
+try {
+	app.listen(port, (): void => {
+		console.log("Connected successfully");
+	});
+} catch (error) {
+	console.error(`Error occured: ${error.message}`);
+}
